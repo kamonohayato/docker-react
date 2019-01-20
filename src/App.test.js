1560929e5c55b5eb812f6e54d24987fef8e71a43
Hayato Kamono <hayatokamono@HayatonoMacBook-puro.local>
2019-01-20 15:58:18 +0900

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+it('renders without crashing1', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing2', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
